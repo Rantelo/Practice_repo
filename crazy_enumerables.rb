326a71554996e1b37f9540f53aa075b7a35c37bb
Roberto Antelo <roberto_antelo@hotmail.com>
2015-09-03 21:03:26 -0500

    Definition of module

---
+++ crazy_enumerables.rb
@@ -0,0 +1,43 @@
+module Enumerable
+# TODO: my_each
+  def my_each
+	end
+
+# TODO: my_each_with_index
+  def my_each_with_index
+  end
+
+# TODO: my_select
+  def my_select
+  end
+# TODO: my_all?
+  def my_all?
+  end
+
+# TODO: my_any?
+  def my_any?
+  end
+
+# TODO: my_none
+  def my_none  
+  end
+
+# TODO: my_count
+  def my_count
+  end
+
+# TODO: my_map
+  def my_map
+  end
+
+# TODO: my_inject
+  def my_inject
+  end
+
+# NOTE: Test #my_inject by creating a method called #multiply_els which multiplies all the elements of the array together by using #my_inject, e.g. multiply_els([2,4,5]) #=> 40
+
+# TODO: Modify your #my_map method to take a proc
+
+# TODO: Modify your #my_map method to take either a proc or a block, executing the block only if both are supplied (in which case it would execute both the block AND the proc).
+
+end
