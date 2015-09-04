75b42dbd6bd6ab644be36013aaba8b18db61e1ea
Roberto Antelo <roberto_antelo@hotmail.com>
2015-09-03 20:38:54 -0500

    Changed sample input

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,7 +15,10 @@ def bubble_sort_by ary
   end
 end
 
-ary = [4,3,78,2,0,32]
+ary       = [4,3,78,2,0,32]
+other_ary = ["hi","hello","hey"]
 
-puts bubble_sort_by(ary){|left, rigth| left-rigth}.join("-") #=> 0-2-3-4-32-78
-puts bubble_sort(ary).join("-")                              #=> 0-2-3-4-32-78
+puts  bubble_sort(ary).join("-")               #=> 0-2-3-4-32-78
+puts  bubble_sort_by(other_ary) {|left, rigth| #=> hi-hey-hello
+        left.length-rigth.length
+      }.join("-")
